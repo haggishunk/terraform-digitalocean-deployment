882375f0054c1c306ea5b409cc4ac0f0b99eabbb
haggishunk <travis@mattera.io>
2022-09-18 12:05:55 -0700

    Deprecated floating ip

--- ip.tf
+++ ip.tf
@@ -1,9 +1,9 @@
-resource "digitalocean_floating_ip" "this" {
+resource "digitalocean_reserved_ip" "this" {
   count  = var.floating_ip_enabled ? 1 : 0
   region = var.region
 }
 
-resource "digitalocean_floating_ip_assignment" "this" {
+resource "digitalocean_reserved_ip_assignment" "this" {
   count      = var.floating_ip_enabled ? 1 : 0
   ip_address = digitalocean_floating_ip.this.0.ip_address
   droplet_id = digitalocean_droplet.this.id
